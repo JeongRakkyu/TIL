51f04a2cd7ca56f02644e590280c313b4cc790b9
Programmer Egg <JeongRakkyu@users.noreply.github.com>
2019-01-01 13:50:27 +0900

    [TIL] Build OpenCV with Android studio [Editor] JeongRakkyu [Desc] Build OpenCV for making a camera filter app.

---
+++ android_studio/OpenCV/app/src/main/java/com/example/rakkyu/opencv/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.rakkyu.opencv;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
